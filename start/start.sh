6d04628ef6f36602c3fc83ed97d91df33c18f34d
Matthieu Delacour <58316083+Tenzix@users.noreply.github.com>
2024-01-10 03:40:19 +0100

    Delete start directory

--- start/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start Fooocus
-python /Fooocus/entry_with_update.py --preset realistic --share &
-
-# Start Python Discord bot
-python /Fooocus-discord-autopublish--docker/autopublish.py
